06933b03fbf6f9222473b2498e36b1ef46784328
David Brown <dmlb2000@gmail.com>
2014-09-27 16:12:32 -0700

    this is more json like building objects.

--- src/gov/pnnl/emsl/PacificaLibrary/PacificaGroupSerializer.java
+++ src/gov/pnnl/emsl/PacificaLibrary/PacificaGroupSerializer.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Type;
 import gov.pnnl.emsl.SWADL.Group;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
@@ -15,8 +16,10 @@ public class PacificaGroupSerializer implements JsonSerializer<Group> {
 	public JsonElement serialize(Group arg0, Type arg1,
 			JsonSerializationContext arg2) {
 		// TODO Auto-generated method stub
-		String groupstr = "{\"name\":\""+arg0.getValue()+"\", \"type\":\""+arg0.getKey()+"\"}";
-		return new JsonPrimitive(groupstr);
+		JsonObject group = new JsonObject();
+		group.add("name", new JsonPrimitive(arg0.getValue()));
+		group.add("type", new JsonPrimitive(arg0.getKey()));
+		return group;
 	}
 
 }
